d41fb652acc2c1032cdce81d568dc2d155725eaf
Pedro Knup <pedro.knup.barros@rabobank.nl>
2024-08-02 10:06:23 +0200

    Make harpoon window wider

--- .config/nvim/lua/pedro/plugins/harpoon.lua
+++ .config/nvim/lua/pedro/plugins/harpoon.lua
@@ -1,7 +1,12 @@
 return {
     "ThePrimeagen/harpoon", -- recent files
     config = function()
-        require("harpoon").setup()
+        require("harpoon").setup({
+            menu = {
+                width = vim.api.nvim_win_get_width(0) - 4,
+            },
+        })
+
         require("telescope").load_extension("harpoon")
 
         vim.keymap.set("n", "<leader>hh", ":lua require('harpoon.ui').toggle_quick_menu()<CR>")

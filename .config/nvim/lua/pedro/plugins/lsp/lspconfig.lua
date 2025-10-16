32c068393cde0ad873e960195eff77e7c393d1a9
Pedro Knup <phknup@gmail.com>
2025-10-16 11:27:37 +0200

    feat: update packages and fix ts LSP set up

--- .config/nvim/lua/pedro/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/pedro/plugins/lsp/lspconfig.lua
@@ -1,6 +1,5 @@
 return {
     "mason-org/mason-lspconfig.nvim",
-    -- version = "1.32.0",
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
@@ -8,24 +7,15 @@ return {
         { "folke/neodev.nvim", opts = {} },
     },
     config = function()
-        -- import lspconfig plugin
-        local lspconfig = require("lspconfig")
-        local util = require("lspconfig.util")
-        -- import mason_lspconfig plugin
         local mason_lspconfig = require("mason-lspconfig")
-
-        -- import cmp-nvim-lsp plugin
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-        local keymap = vim.keymap -- for conciseness
+        local keymap = vim.keymap
         vim.api.nvim_create_autocmd("LspAttach", {
             group = vim.api.nvim_create_augroup("UserLspConfig", {}),
             callback = function(ev)
-                -- Buffer local mappings.
-                -- See `:help vim.lsp.*` for documentation on any of the below functions
                 local opts = { buffer = ev.buf, silent = true }
 
-                -- set keybinds
                 opts.desc = "Show LSP references"
                 keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
 
@@ -71,82 +61,69 @@ return {
         local capabilities = cmp_nvim_lsp.default_capabilities()
 
         -- Change the Diagnostic symbols in the sign column (gutter)
-        -- (not in youtube nvim video)
-        local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+        local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
         for type, icon in pairs(signs) do
             local hl = "DiagnosticSign" .. type
             vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
         end
 
+        local default_config = {
+            capabilities = capabilities,
+        }
+
+        vim.lsp.config("*", default_config)
+
+        -- configure graphql language server
+        vim.lsp.config.graphql = vim.tbl_extend("force", default_config, {
+            filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+        })
+
+        -- configure emmet language server
+        vim.lsp.config.emmet_ls = vim.tbl_extend("force", default_config, {
+            filetypes = {
+                "html",
+                "typescriptreact",
+                "javascriptreact",
+                "css",
+                "sass",
+                "scss",
+                "less",
+                "svelte",
+            },
+        })
+
+        -- configure lua server (with special settings)
+        vim.lsp.config.lua_ls = vim.tbl_extend("force", default_config, {
+            settings = {
+                Lua = {
+                    -- make the language server recognize "vim" global
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                    completion = {
+                        callSnippet = "Replace",
+                    },
+                },
+            },
+        })
+
+        -- configure typescript server (with special settings)
+        vim.lsp.config.ts_ls = vim.tbl_extend("force", default_config, {
+            root_dir = vim.fs.root(0, { "tsconfig.json", "jsconfig.json" }),
+            handlers = {
+                -- pick the first response to a go to definition response. that way we go straight to the
+                -- source definition without needing to choose from the type definition .d.ts file
+                ["textDocument/definition"] = function(err, result, ...)
+                    result = vim.tbl_islist(result) and result[1] or result
+                    vim.lsp.handlers["textDocument/definition"](err, result, ...)
+                end,
+            },
+        })
+
         mason_lspconfig.setup_handlers({
             -- default handler for installed servers
             function(server_name)
-                lspconfig[server_name].setup({
-                    capabilities = capabilities,
-                })
-            end,
-            ["graphql"] = function()
-                -- configure graphql language server
-                lspconfig["graphql"].setup({
-                    capabilities = capabilities,
-                    filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-                })
-            end,
-            ["emmet_ls"] = function()
-                -- configure emmet language server
-                lspconfig["emmet_ls"].setup({
-                    capabilities = capabilities,
-                    filetypes = {
-                        "html",
-                        "typescriptreact",
-                        "javascriptreact",
-                        "css",
-                        "sass",
-                        "scss",
-                        "less",
-                        "svelte",
-                    },
-                })
-            end,
-            ["lua_ls"] = function()
-                -- configure lua server (with special settings)
-                lspconfig["lua_ls"].setup({
-                    capabilities = capabilities,
-                    settings = {
-                        Lua = {
-                            -- make the language server recognize "vim" global
-                            diagnostics = {
-                                globals = { "vim" },
-                            },
-                            completion = {
-                                callSnippet = "Replace",
-                            },
-                        },
-                    },
-                })
-            end,
-            ["tsserver"] = function()
-                -- configure typescript server (with special settings)
-                lspconfig["tsserver"].setup({
-                    capabilities = capabilities,
-                    settings = {
-                        typescript = {
-                            -- make the language server recognize "vim" global
-                            diagnostics = {
-                                globals = { "vim" },
-                            },
-                        },
-                    },
-                    root_dir = util.root_pattern("tsconfig.json", "jsconfig.json"),
-                    handlers = {
-                        -- pick the first response to a go to definition response. that way we go straight to the
-                        -- source definition without needing to choose from the type definition .d.ts file
-                        ["textDocument/definition"] = function(err, result, ...)
-                            result = vim.tbl_islist(result) and result[1] or result
-                            vim.lsp.handlers["textDocument/definition"](err, result, ...)
-                        end,
-                    },
-                })
+                vim.lsp.enable(server_name)
             end,
         })
     end,
